91ea06905335253d80123f98f0b5e16d5ea30d27
jkcnl001 <jkcnl001@jkcnl001deiMac.lan>
2019-10-16 20:51:40 +0800

    update

--- assets/Script/Ball.ts
+++ assets/Script/Ball.ts
@@ -9,9 +9,9 @@
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
 const { ccclass, property } = cc._decorator;
-
+import Config from "./Config"
 @ccclass
-export default class NewClass extends cc.Component {
+export default class Ball extends cc.Component {
     @property(cc.RigidBody)
     rigidBody: cc.RigidBody = null
     isTouchedGround: Boolean = false
@@ -25,6 +25,27 @@ export default class NewClass extends cc.Component {
         if (this.isTouchedGround) {
             this.rigidBody.active = false
             this.rigidBody.linearVelocity = cc.Vec2.ZERO;
+             /*记录路径点*/
+             let pathPos = [];
+             pathPos.push(this.node.position);
+             pathPos.push(cc.v2(349, -498))
+             pathPos.push(cc.v2(338, 608))
+             pathPos.push(cc.v2(162, 557))
+             this.node.runAction(cc.sequence(
+                cc.cardinalSplineTo(1, pathPos, 0.9),
+                cc.callFunc(function () {
+                    this.rigidBody.active = true;
+                    this.node.group = Config.groupBallInRecycle;
+                    this.main.recycleBall();
+                }.bind(this))
+            ))
+            this.isTouchedGround = false;
         }
     }
+   /*小球发生碰撞时*/
+   onBeginContact(contact, selfCollider, otherCollider) {
+    if(otherCollider.node.name == 'ground'){
+        this.isTouchedGround = true;
+    }
+   }
 }
